3eb14fb647dbb872551c3b6c99d6dfd047226e25
negswell <negiaditya16@gmail.com>
2024-06-28 12:50:03 +0530

    fixed backend api url

--- next.config.mjs
+++ next.config.mjs
@@ -4,22 +4,7 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL:
       process.env.NODE_ENV == "development"
         ? "http://127.0.0.1:8000"
-        : "http://34.19.77.36",
-  },
-  async headers() {
-    return [
-      {
-        // Define Content-Security-Policy header
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self' https: http: data: 'unsafe-inline' 'unsafe-eval';",
-          },
-        ],
-      },
-    ];
+        : "https://chatbot.talent-labs.com",
   },
 };
 
